7dd500ebd9ae4eb54f9c2e7b482fd5ac77c892ad
Tobsi <tsh40884137@gmail.com>
2022-05-02 12:35:19 +0200

    Test

--- MovieDataViewer/Form1.Designer.cs
+++ MovieDataViewer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MovieDataViewer
 {
-    partial class Form1
+    partial class MovieViewer
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MovieViewer));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCsvMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.TempLOGO = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.ReadOnlyChecked = true;
+            this.openFileDialog1.ShowReadOnly = true;
+            this.openFileDialog1.SupportMultiDottedExtensions = true;
+            this.openFileDialog1.Title = "Open .csv";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(251)))), ((int)(((byte)(244)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeMenuItem,
+            this.openToolStripMenuItem,
+            this.showMenuItem,
+            this.exitMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeMenuItem
+            // 
+            this.homeMenuItem.Name = "homeMenuItem";
+            this.homeMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.homeMenuItem.Text = "Home";
+            this.homeMenuItem.Click += new System.EventHandler(this.homeToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCsvMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // openCsvMenuItem
+            // 
+            this.openCsvMenuItem.Name = "openCsvMenuItem";
+            this.openCsvMenuItem.Size = new System.Drawing.Size(91, 22);
+            this.openCsvMenuItem.Text = "csv";
+            this.openCsvMenuItem.Click += new System.EventHandler(this.csvToolStripMenuItem_Click);
+            // 
+            // showMenuItem
+            // 
+            this.showMenuItem.Name = "showMenuItem";
+            this.showMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.showMenuItem.Text = "Show";
+            this.showMenuItem.Click += new System.EventHandler(this.showMenuItem_Click);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitMenuItem.Text = "Exit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // TempLOGO
+            // 
+            this.TempLOGO.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TempLOGO.AutoSize = true;
+            this.TempLOGO.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TempLOGO.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.TempLOGO.Location = new System.Drawing.Point(86, 219);
+            this.TempLOGO.Name = "TempLOGO";
+            this.TempLOGO.Size = new System.Drawing.Size(228, 47);
+            this.TempLOGO.TabIndex = 1;
+            this.TempLOGO.Text = "Movie Viewer";
+            this.TempLOGO.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.TempLOGO.Click += new System.EventHandler(this.TempLOGO_Click);
+            // 
+            // MovieViewer
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(384, 485);
+            this.Controls.Add(this.TempLOGO);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MovieViewer";
+            this.Text = "MovieViewer (0.0.1)";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem showToolStripMenuItem;
+        private ToolStripMenuItem MenuBar;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private ToolStripMenuItem homeMenuItem;
+        private ToolStripMenuItem openCsvMenuItem;
+        private ToolStripMenuItem showMenuItem;
+        private ToolStripMenuItem exitMenuItem;
+        private Label TempLOGO;
     }
 }
--- MovieDataViewer/Form1.cs
+++ MovieDataViewer/Form1.cs
@@ -1,10 +1,159 @@
 namespace MovieDataViewer
 {
-    public partial class Form1 : Form
+    public partial class MovieViewer : Form
     {
-        public Form1()
+        public MovieViewer()
         {
             InitializeComponent();
         }
+        public int alpha = 255;
+        public bool showLogo = true;
+        private void colorTransition(object sender)
+        {
+            /*if (showLogo)
+            {
+                while (alpha > 255)
+                {
+                    if(alpha >= 255)
+                    {
+                        TempLOGO.ForeColor = Color.FromArgb(255, 255, 192, 128);
+                    }
+                    else
+                    {
+                        TempLOGO.Visible = true;
+                        TempLOGO.Enabled = true;
+                        alpha++;
+                        TempLOGO.ForeColor = Color.FromArgb(alpha, 255, 192, 128);
+                    }
+                }
+                /*if(alpha <= 254)
+                {
+                    TempLOGO.Visible = true;
+                    TempLOGO.Enabled = true;
+                    alpha++;
+                    TempLOGO.ForeColor = Color.FromArgb(alpha, 255, 192, 128);
+                }
+                else
+                {
+                    TempLOGO.ForeColor = Color.FromArgb(255, 255, 192, 128);
+                }*/
+
+            /*}
+            else
+            {
+                TempLOGO.Visible = false;
+                TempLOGO.Enabled = false;
+                while (alpha < 0)
+                {
+                    if(alpha <= 0)
+                    {
+                        TempLOGO.Visible = false;
+                        TempLOGO.Enabled = false;
+                        TempLOGO.ForeColor = Color.FromArgb(0, 255, 192, 128);
+                    }
+                    else
+                    {
+                        alpha--;
+                        TempLOGO.ForeColor = Color.FromArgb(alpha, 255, 192, 128);
+                    }
+                    TempLOGO.Visible = false;
+                    TempLOGO.Enabled = false;
+                }
+                /*if (alpha >= 1)
+                {
+                    alpha--;
+                    TempLOGO.ForeColor = Color.FromArgb(alpha, 255, 192, 128);
+                }
+                else
+                {
+                    TempLOGO.Visible = false;
+                    TempLOGO.Enabled = false;
+                    TempLOGO.ForeColor = Color.FromArgb(0, 255, 192, 128);
+                }*/
+            //}
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void csvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Might be able to hide the unused components. 
+
+            if (TempLOGO.Visible == false)
+            {
+
+                showLogo = true;
+                //colorTransition(TempLOGO);
+            }
+            else
+            {
+                showLogo = false;
+                //colorTransition(TempLOGO);
+
+            }
+            /*Form aboutForm = new AboutBox1();
+            aboutForm.ShowDialog();*/
+            Form form = new AboutBox1();
+            form.Location = this.Location;
+            form.StartPosition = FormStartPosition.Manual;
+            form.FormClosing += delegate { this.Show(); };
+            form.Show();
+            this.Hide();
+
+            /*MessageBox.Show("Home");
+            if(TempLOGO.Visible == false)
+            {
+                TempLOGO.Visible = true;
+                TempLOGO.Enabled = true;
+                TempLOGO.ForeColor = Color.FromArgb(255, 255, 192, 128);
+            }
+            else
+            {
+                TempLOGO.Visible = false;
+                TempLOGO.Enabled = false;
+                TempLOGO.ForeColor = Color.FromArgb(255, 255, 192, 128);
+            }*/
+
+        }
+
+        private void showMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void TempLOGO_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
